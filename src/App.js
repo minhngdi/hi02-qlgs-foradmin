15ab7807f3f19fe3adf008807a137fa559047c14
Nguyen Dinh Minh <minh.ngdi@gmail.com>
2020-05-25 22:37:43 +0700

    Init react with antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Layout, Button} from 'antd';
 import './App.css';
+
+const {Header} = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Layout>
+          <Header className='header'>
+            <h1 className='title'>Quan Ly Giam Sat</h1>
+         </Header>
+        </Layout>
+        <Button>
+          Button Test
+        </Button>
     </div>
   );
 }
