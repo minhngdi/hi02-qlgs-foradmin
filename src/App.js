69817726512788bf642b4eb181e4512c5d4314df
Nguyen Dinh Minh <minh.ngdi@gmail.com>
2020-05-26 14:13:06 +0700

    Init file structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {Layout, Button} from 'antd';
 import './App.css';
+import Dashboard from './containers/Dashboard';
 
-const {Header} = Layout;
 
 function App() {
   return (
     <div className="App">
-        <Layout>
-          <Header className='header'>
-            <h1 className='title'>Quan Ly Giam Sat</h1>
-         </Header>
-        </Layout>
-        <Button>
-          Button Test
-        </Button>
+      <Dashboard />
     </div>
   );
 }
---
+++ src/containers/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import {Layout, Button} from 'antd';
+import Header from '../../components/Header';
+
+const Dashboard = (props) => {
+  return(
+    <div>
+        <h1>Dashboard</h1>
+        <Header />
+    </div>
+   )
+
+ }
+
+export default Dashboard; 
