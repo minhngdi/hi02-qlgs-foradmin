35a050ce1f6e698720e91cd2f92e1a7a4961d543
nguyennd-0051 <nguyen.duy.nguyen@sun-asterisk.com>
2020-05-27 03:25:05 +0700

    add inspection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './containers/Dashboard';
+import Inspection from "./containers/Inspection";
 
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+      <br />
+      <Inspection />
     </div>
   );
 }
