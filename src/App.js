d8ccfad1c274414632f8ce3d2dde14671817f649
vp1927 <phamvietcuong2704@gmail.com>
2020-05-27 00:52:06 +0700

    sample form, will update

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import React from 'react';
 import './App.css';
 import Dashboard from './containers/Dashboard';
 
-import CreateWarning from './Warning/CreateWarning'
-const {Header} = Layout;
+import CreateWarning from './containers/Warning/CreateWarning'
 
 function App() {
-  
   return (
     <div className="App">
-        <Layout>
-          <Header className='header'>
-            <h1 className='title'>Quan Ly Giam Sat</h1>
-         </Header>
-        </Layout>
-        <Button>
-          Button Test
-        </Button>
       <CreateWarning />
-=======
-      <Dashboard />
->>>>>>> 03ff6c8fc072cb8598fa3a6accc1eead8d3cd504
     </div>
   );
 }
+      // <Dashboard />
 
 export default App;
